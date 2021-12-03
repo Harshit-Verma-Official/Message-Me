a21c3a601364ca242f1ebfc73eb27e3d8eb6ab63
Harshit Verma <harshit.verma@unthinkable.co>
2021-12-03 13:26:03 +0530

    Add favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,9 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import '@doabit/semantic-ui-sass'
+
+$(document).on('turbolinks:load', () => {
+    $('.ui.dropdown').dropdown();
+})
